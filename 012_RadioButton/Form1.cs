03382748bdea1b41137671040e8701a5a9deefd7
BeeEye Dmu <bikang@konyang.ac.kr>
2025-03-31 13:20:55 +0900

    Add files via upload

---
+++ 012_RadioButton/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _012_RadioButton
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void btnSubmit_Click(object sender, EventArgs e)
+    {
+      string s = "";
+
+      if(rbKorea.Checked ) 
+        s += "국적 : 대한민국\n";
+      else if(rbChina.Checked )
+        s += "국적 : 중국\n";
+      else if (rbJapan.Checked)
+        s += "국적 : 일본\n";
+      else if (rbOthers.Checked)
+        s += "국적 : 그 외의 국가\n";
+
+      if (rbMale.Checked)
+        s += "성별 : 남성";
+      else if (rbFemale.Checked)
+        s += "성별 : 여성";
+
+      MessageBox.Show(s, "결과");
+
+    }
+  }
+}
---
+++ 013_TempConv/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _013_TempConv
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      if (txtC.Text == "" && txtF.Text == "")
+        return;
+      if(txtC.Text != "")
+      {
+        double f = double.Parse(txtC.Text) * 9 / 5 + 32;
+        txtF.Text = f.ToString("#.##");
+      }
+      else if(txtF.Text != "")
+      {
+        double c = (double.Parse(txtF.Text) - 32) * 5 / 9;
+        txtC.Text = c.ToString("#.##");
+      }
+    }
+  }
+}
